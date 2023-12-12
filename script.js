35094b2cd07b1b681a639c447f400a6c8c1fc7cd
lhondikoi <l.hondikoi@gmail.com>
2023-12-12 18:13:22 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+advHolder = document.querySelector('#advice')
+app = document.querySelector('#app')
+btn = document.querySelector('.btn')
+
+function getNewAdvice() {
+    btn.style.opacity = 0
+    advHolder.style.opacity = 0
+    setTimeout(()=> {
+        btn.style.opacity = 1
+        advHolder.style.opacity = 1
+    }, 1000)
+    fetch('https://api.adviceslip.com/advice')
+    .then(r => r.json())
+    .then(d => {
+        advHolder.innerText = d.slip.advice;
+    })
+    .catch((e)=> {
+        alert('There was an fetching the advice!')
+    })
+}
+
+window.addEventListener('load', () => {
+    getNewAdvice();
+})
